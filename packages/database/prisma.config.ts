b171814589608f046d6324ed2b6fcdbd63c4c353
Srinidhi-4419 <kulkarnisrinidhi85@gmail.com>
2026-07-07 22:10:06 +0530

    initial setup of folders and writing schemas

---
+++ packages/database/prisma.config.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: "postgresql://postgres:postgres@localhost:5433/reliable_job_queue?schema=public",
+  },
+});
